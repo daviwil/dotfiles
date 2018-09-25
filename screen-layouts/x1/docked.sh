6a5ca8cd62adacfa229f7a3e8305a5aef80181aa
David Wilson <daviwil@github.com>
2018-09-25 07:23:24 -0700

    Add arandr screen layout scripts for X1 Carbon

---
+++ screen-layouts/x1/docked.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 3840x416 --rotate normal --output DP1 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP2 --off
