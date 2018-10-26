231a023a05c63464d6f8cf86310128d3e9120f5e
David Wilson <daviwil@github.com>
2018-10-25 18:22:01 -0700

    Update customize.el

--- emacs/config/customize.el
+++ emacs/config/customize.el
@@ -8,7 +8,8 @@
  '(c-basic-offset (quote set-from-style))
  '(custom-safe-themes
    (quote
-    ("72085337718a3a9b4a7d8857079aa1144ea42d07a4a7696f86627e46ac52f50b" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "5e2dc1360a92bb73dafa11c46ba0f30fa5f49df887a8ede4e3533c3ab6270e08" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+    ("e7b49145d311e86da34a32a7e1f73497fa365110a813d2ecd8105eaa551969da" "5dc98c649900306c934d94c0113e54702f3904bf3784f553a33381d74b3b7e1d" "8d805143f2c71cfad5207155234089729bb742a1cb67b7f60357fdd952044315" "72085337718a3a9b4a7d8857079aa1144ea42d07a4a7696f86627e46ac52f50b" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "5e2dc1360a92bb73dafa11c46ba0f30fa5f49df887a8ede4e3533c3ab6270e08" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(electric-pair-mode t)
  '(erc-modules
    (quote
     (autoaway autojoin button completion fill irccontrols keep-place list match menu move-to-prompt netsplit networks noncommands notify notifications readonly ring smiley stamp track)))
@@ -16,7 +17,7 @@
  '(global-magit-file-mode t)
  '(org-agenda-files
    (quote
-    ("/home/daviwil/Notes/Projects.org" "/home/daviwil/Notes/Journal/2018/2018-10-October.org")))
+    ("/home/daviwil/Notes/Inbox.org" "/home/daviwil/Notes/Habits.org" "/home/daviwil/Notes/Calendar.org" "/home/daviwil/Notes/Projects.org")))
  '(org-agenda-hide-tags-regexp "agenda\\|process")
  '(org-agenda-prefix-format
    (quote
@@ -26,6 +27,7 @@
      (tags . " %i %-18:c")
      (search . " %i %-18:c"))))
  '(org-agenda-tags-column -120)
+ '(org-bullets-bullet-list (quote ("◉" "○" "●" "○" "●" "○" "●")))
  '(org-cycle-separator-lines 1)
  '(org-ellipsis " ->")
  '(org-hide-block-startup t)
@@ -34,7 +36,15 @@
  '(org-startup-folded (quote content))
  '(package-selected-packages
    (quote
-    (magithub zoom helm-projectile expand-region system-packages helm-pass buffer-flip use-package-chords mastodon counsel-spotify ivy-pass persp-mode erc-image erc-hl-nicks calfw-org calfw org-wild-notifier org-alert elfeed md4rd org-gcal eshell-prompt-extras mu4e-alert default-text-scale offlineimap notmuch dired-rainbow ghub+ org evil-escape wttrin circe-notifications nvm lsp-javascript-typescript js2-mode emms-player-mpv magit-gh-pulls sauron erc-status-sidebar dashboard eyebrowse lsp-ui f multi-term color-theme-sanityinc-tomorrow noctilux-theme nord-theme dakrone-theme smex evil-collection counsel-projectile cargo lsp-rust rust-mode avy tide ranger diminish org-bullets all-the-icons-ivy counsel ivy slack emojify general which-key twittering-mode emms restclient projectile smart-mode-line jabber nix-mode csharp-mode yasnippet impatient-mode web-mode markdown-mode fsharp-mode git-annex magit-annex git-gutter magit cider clojure-mode helm-company company ido-vertical-mode flx-ido rainbow-delimiters smartparens helm-themes helm-package helm deft org-journal org-pomodoro evil-nerd-commenter evil badger-theme use-package)))
+    (helm-ag ag yaml-mode org-evil exec-path-from-shell shx nvm\.el circe zeno-theme magithub zoom helm-projectile expand-region system-packages helm-pass buffer-flip use-package-chords mastodon counsel-spotify ivy-pass persp-mode erc-image erc-hl-nicks calfw-org calfw org-wild-notifier org-alert elfeed md4rd org-gcal eshell-prompt-extras mu4e-alert default-text-scale offlineimap notmuch dired-rainbow ghub+ org evil-escape wttrin circe-notifications nvm lsp-javascript-typescript js2-mode emms-player-mpv magit-gh-pulls sauron erc-status-sidebar dashboard eyebrowse lsp-ui f multi-term color-theme-sanityinc-tomorrow noctilux-theme nord-theme dakrone-theme smex evil-collection counsel-projectile cargo lsp-rust rust-mode avy tide ranger diminish org-bullets all-the-icons-ivy counsel ivy slack emojify general which-key twittering-mode emms restclient projectile smart-mode-line jabber nix-mode csharp-mode yasnippet impatient-mode web-mode markdown-mode fsharp-mode git-annex magit-annex git-gutter magit cider clojure-mode helm-company company ido-vertical-mode flx-ido rainbow-delimiters smartparens helm-themes helm-package helm deft org-journal org-pomodoro evil-nerd-commenter evil badger-theme use-package)))
+ '(safe-local-variable-values
+   (quote
+    ((projectile-project-compilation-dir)
+     (projectile-project-compilation-dir \,@ atom-project-dir)
+     (projectile-project-compilation-dir . atom-project-dir)
+     (projectile-project-compilation-cmd . "script/build")
+     (projectile-project-compilation-dir . "~/Projects/Code/atom")
+     (projectile-project-name . "Atom"))))
  '(tab-width 4)
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
@@ -57,7 +67,9 @@
      (320 . "#DCA432")
      (340 . "#ebcb8b")
      (360 . "#B4EB89"))))
- '(vc-annotate-very-old-color nil))
+ '(vc-annotate-very-old-color nil)
+ '(zoom-mode nil nil (zoom))
+ '(zoom-size (quote (0.618 . 0.618))))
 
 ; '(default ((t (:foreground "#F6F3E8" :background "#171717"))))
 ; '(org-level-1 ((t (:background "gray13" :foreground "#8AC6F2" :height 1.2)))))
