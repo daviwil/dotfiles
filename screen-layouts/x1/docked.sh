5ca86c3c5b9c7c461412635b652a5a7782dae9dc
David Wilson <daviwil@github.com>
2019-01-02 16:13:53 -0800

    Take i3-specific commands out of screen layout scripts

--- screen-layouts/x1/docked.sh
+++ screen-layouts/x1/docked.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 3840x416 --rotate normal --output DP1 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP2 --off
 
-i3-msg -q "workspace --no-auto-back-and-forth number 1; move workspace to output primary; workspace back_and_forth"
-i3-msg -q "workspace --no-auto-back-and-forth number 2; move workspace to output primary; workspace back_and_forth"
-i3-msg -q "workspace --no-auto-back-and-forth number 3; move workspace to output primary; workspace back_and_forth"
+# TODO: Split this into a separate script for use in i3
+#i3-msg -q "workspace --no-auto-back-and-forth number 1; move workspace to output primary; workspace back_and_forth"
+#i3-msg -q "workspace --no-auto-back-and-forth number 2; move workspace to output primary; workspace back_and_forth"
+#i3-msg -q "workspace --no-auto-back-and-forth number 3; move workspace to output primary; workspace back_and_forth"
 
 nitrogen --restore
