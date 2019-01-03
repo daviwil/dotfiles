c71190fa8c4216e50e0443d82ab87331bf1599fc
David Wilson <daviwil@github.com>
2019-01-03 09:53:55 -0800

    Enable screen locking in exwm

--- emacs/exwm/start-exwm.sh
+++ emacs/exwm/start-exwm.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
+# Start authentication daemons
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
+
+# Make things look nice
 compton --config ~/.dotfiles/emacs/compton.conf &
 nitrogen --restore &
+
+# Start Xfce's settings and power managers
 xfsettingsd &
 xfce4-power-manager &
-pasystray &
+
+# Start up Syncthing for file synchronization
 syncthing-gtk --minimized &
+
+# Load system tray apps for sound and bluetooth devices
 blueman-applet &
+pasystray &
+
+# Enable Manjaro update checks
 pamac-tray &
 
+# Enable screen locking on suspend
+xss-lock -- slock &
+
+# We're in Emacs, yo
 export VISUAL=emacsclient
 export EDITOR="$VISUAL"
 
+# Fire it up
 emacs
