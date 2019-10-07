335d9a1500a78db1c31a8c311f63e30a31433319
David Wilson <david@daviwil.com>
2019-10-07 06:13:19 -0700

    Auto-zoom pages on load in Next

--- next/.config/next/init.lisp
+++ next/.config/next/init.lisp
@@ -1,3 +1,13 @@
 (in-package :next)
 
 (add-to-default-list 'vi-normal-mode 'buffer 'default-modes)
+
+(defun my-buffer-defaults (buffer)
+  (setf (zoom-ratio-default buffer) 1.6)
+  (unzoom-page :buffer buffer))          ; Needed for non-web-mode buffers.
+
+(defun my-interface-defaults ()
+  (hooks:add-to-hook (hooks:object-hook *interface* 'buffer-make-hook)
+                     #'my-buffer-defaults))
+
+(hooks:add-to-hook '*after-init-hook* #'my-interface-defaults)
