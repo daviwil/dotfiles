adf3b07ae7cfc8a536b1b933e723b5c897868464
David Wilson <david@daviwil.com>
2019-10-15 05:28:30 -0700

    Add local Guix repo channel configuration

--- guix/channels.scm
+++ guix/channels.scm
@@ -2,3 +2,16 @@
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix"))
        %default-channels)
+
+;; The following channel list can be used when testing patches
+;; to services from a local clone of the Guix repo.  Just make
+;; sure to commit the changes to a branch and refer to that
+;; branch by name in the 'guix channel entry below:
+
+;; (list (channel
+;;         (name 'nonguix)
+;;         (url "https://gitlab.com/nonguix/nonguix"))
+;;       (channel
+;;         (name 'guix)
+;;         (branch "fix-xfce-power-manager-polkit")
+;;         (url "file:///home/daviwil/Projects/Code/guix")))
