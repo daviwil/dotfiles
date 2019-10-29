86e7b140f1f9b39c5825b54d2bc0e7aa4f8b4455
David Wilson <david@daviwil.com>
2019-10-28 17:15:24 -0700

    Customize minibuffer style

--- next/.config/next/init.lisp
+++ next/.config/next/init.lisp
@@ -17,11 +17,57 @@
 (add-to-default-list 'vi-normal-mode 'buffer 'default-modes)
 (add-to-default-list 'dw-mode 'buffer 'default-modes)
 
+(defun my-minibuffer-defaults (minibuffer)
+  (setf (minibuffer-style minibuffer)
+        (cl-css:css
+            '((* :font-family "monospace,monospace")
+              (body :border-top "4px solid dimgray"
+                    :margin "0"
+                    :padding "0 6px"
+                    :background-color "#1c1f26")
+              ("#container" :display "flex"
+                            :flex-flow "column"
+                            :height "100%")
+              ("#input" :padding "6px 0"
+                        :border-bottom "solid 1px lightgray")
+              ("#completions" :flex-grow "1"
+                              :overflow-y "auto"
+                              :overflow-x "auto"
+                              :background-color "#1c1f26"
+                              :color "white")
+              ("#cursor" :background-color "gray"
+                         :color "white")
+              ("#prompt" :padding-right "4px"
+                         :color "lightgreen"
+                         :background-color "#1c1f26")
+              (ul :list-style "none"
+                  :padding "0"
+                  :margin "0")
+              (li :padding "2px")
+              (.marked :background-color "darkgray"
+                       :font-weight "bold"
+                       :color "white")
+                ;; .selected must be set _after_ .marked so
+                ;; that it overrides its attributes since the
+                ;; candidate can be both marked and selected.
+              (.selected :background-color "gray"
+                         :color "white")))))
+
+  ;; TODO: How do I set this?
+  ;; (setf (minibuffer-line-style minibuffer)
+  ;;   (cl-css:css
+  ;;     `((* :font-size ,(minibuffer-font-size minibuffer)
+  ;;          :line-height ,(minibuffer-line-height minibuffer)
+  ;;          :color "white"
+  ;;          :background-color "#1c1f26")))))
+
 (defun my-buffer-defaults (buffer)
   (setf (zoom-ratio-default buffer) 1.6)
   (unzoom-page :buffer buffer))          ; Needed for non-web-mode buffers.
 
 (defun my-interface-defaults ()
+  (hooks:add-to-hook (hooks:object-hook *interface* 'minibuffer-make-hook)
+                     #'my-minibuffer-defaults)
   (hooks:add-to-hook (hooks:object-hook *interface* 'buffer-make-hook)
                      #'my-buffer-defaults))
 
