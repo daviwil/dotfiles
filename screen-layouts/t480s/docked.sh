5dda40216e95cf27997938c32af8c22f3c4b588e
David Wilson <david@daviwil.com>
2019-10-30 11:38:40 -0700

    Add screen layout script for ThinkPad T480s

---
+++ screen-layouts/t480s/docked.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --off
