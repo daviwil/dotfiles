4aab1583e91ed4077cf2d5098a7851c5f3edfb7e
David Wilson <david@daviwil.com>
2019-11-02 20:21:49 -0700

    emacs: Write out Emacs init duration at startup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,12 @@
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 ;; Initialize package sources
 (require 'package)
 
