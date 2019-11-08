2887a96b17f79fd9c4aee4ac2a171fad67d6ca8b
David Wilson <david@daviwil.com>
2019-11-08 06:03:18 -0800

    emacs: Add initial attempt at a URL opener for Vimb with Ivy

---
+++ .emacs.d/elisp/dw-vimb.el
@@ -0,0 +1,15 @@
+(setq dw/open-url-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<return>") 'ivy-immediate-done)
+    map))
+
+(defun dw/open-url ()
+  (interactive)
+  (let ((history-items
+          (with-temp-buffer
+            (insert-file-contents "~/.config/vimb/history")
+            (split-string (buffer-string) "\n" t))))
+    (ivy-read "Open URL: " (remove-duplicates history-items :test #'string-equal)
+              :keymap dw/open-url-map
+              :action (lambda (item)
+                        (start-process "vimb" nil "vimb" (car (split-string item (string ?\t))))))))
