9aaa28af9de87b2a65bc6aabc943aafad46028da
David Wilson <david@daviwil.com>
2019-12-19 11:08:02 -0800

    guix: Add new browsers.scm profile for ungoogled-chromium

---
+++ .config/guix/manifests/browsers.scm
@@ -0,0 +1,5 @@
+;; This file contains the list of browsers I use occasionally so that I don't
+;; need to install their updates when unnecessary.
+
+(specifications->manifest
+ '("ungoogled-chromium"))
