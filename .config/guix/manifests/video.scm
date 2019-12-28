3ad619c778918c9986c9fb6bb8b7411d3d19b7e3
David Wilson <david@daviwil.com>
2019-12-28 05:36:59 -0800

    guix: Add video.scm manifest for video creation tools

---
+++ .config/guix/manifests/video.scm
@@ -0,0 +1,12 @@
+;; Video creation tools
+
+(specifications->manifest
+ '(;; Screen Capture and Streaming
+   "obs"
+
+   ;; Video Editing
+   "kdenlive"
+   "breeze-icons" ;; kdenlive
+   "ffmpeg"       ;; kdenlive
+   "blender"
+   "openshot"))
