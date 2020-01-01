5a4d7627b0eed6bc13409d0975ae13fa9e4b6580
David Wilson <david@daviwil.com>
2020-01-01 05:04:36 -0800

    guix: Add audio plugin and tool packages to music.scm

--- .config/guix/manifests/music.scm
+++ .config/guix/manifests/music.scm
@@ -3,15 +3,25 @@
 (specifications->manifest
  '(;; JACK tools
    "jack"
+   "jack2"
    "jack-keyboard"
    "qjackctl"
+   "patchage"
 
    ;; DAWs
    "ardour"
    "qtractor"
-   "lmms"
+
+   ;; Guitar
+   "guitarix"
+   "guitarix-lv2"
+
+   ;; Effects
+   "calf"
+   "g2reverb"
 
    ;; Synths
+   "helm"
    "amsynth"
    "avldrums-lv2"
    "zynaddsubfx"))
