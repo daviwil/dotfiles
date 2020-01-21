f299274c211b1b39c3f09087054cb27662b9025a
David Wilson <david@daviwil.com>
2020-01-21 09:39:14 -0800

    polybar: Improve Spotify player status module

---
+++ .config/polybar/player-status.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+status="$(playerctl -p spotify status 2>&1)"
+if [ "$status" != "No players found" ]
+then
+  artist="$(playerctl -p spotify metadata artist)"
+  if [ "$artist" != "" ]
+  then
+    echo " $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
+  else
+    # Clear any string that was previously displayed
+    echo ""
+  fi
+else
+  # Clear any string that was previously displayed
+  echo ""
+fi
