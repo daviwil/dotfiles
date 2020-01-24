0b760067f107c8d91a1b106bc4c143c4169e7762
David Wilson <david@daviwil.com>
2020-01-23 16:54:41 -0800

    update-screens: Add commented out bit for home HDMI docking

--- .bin/update-screens.sh
+++ .bin/update-screens.sh
@@ -7,6 +7,8 @@ case $(hostname) in
         ;;
 
     davinci)
+        # Temporary: this is for docking my laptop at home with HDMI!
+        #xrandr --output HDMI-2 --mode 3840x2160 --pos 0x0 --scale 0.6x0.6 --primary --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 2304x216 --rotate normal --output DP-2 --off
         xrandr --output eDP-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --off
         ;;
 
