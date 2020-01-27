f114c1aa899a108dfba1e0928b5197c5a712539a
David Wilson <david@daviwil.com>
2020-01-27 09:54:11 -0800

    bin: Add sync-dotfiles to sync dotfiles repo and tangle config files

---
+++ .emacs.d/tangle-dotfiles.el
@@ -0,0 +1,8 @@
+(require 'org)
+
+(let* ((dotfiles-path (expand-file-name "~/.dotfiles"))
+       (org-files (directory-files dotfiles-path nil "\\.org$")))
+  (dolist (org-file org-files)
+    (unless (equal org-file "README.org")
+      (message "\n\033[1;32mUpdating %s\033[0m\n" org-file)
+      (org-babel-tangle-file (expand-file-name org-file dotfiles-path)))))
