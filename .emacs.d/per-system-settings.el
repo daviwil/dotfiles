d700fad6a656d6fde30e14ff203f6587b131be5e
David Wilson <david@daviwil.com>
2020-01-28 10:51:40 -0800

    emacs: Add .emacs.d/per-system-settings.el to source control

---
+++ .emacs.d/per-system-settings.el
@@ -0,0 +1,22 @@
+(setq dw/system-settings
+  (map-merge
+    'list
+    '((desktop/dpi . 180)
+      (polybar/height . 35)
+      (polybar/font-0-size . 18)
+      (polybar/font-1-size . 14)
+      (polybar/font-2-size . 20)
+      (dunst/font-size . 20)
+      (dunst/max-icon-size . 64))
+    
+    (when (equal system-name "zerocool"))
+    
+    (when (equal system-name "davinci")
+      '((desktop/dpi . 130)
+        (polybar/height . 25)
+        (polybar/font-0-size . 12)
+        (polybar/font-1-size . 8)
+        (polybar/font-2-size . 14)
+        (dunst/font-size . 14)
+        (dunst/max-icon-size . 64)))
+    ))
