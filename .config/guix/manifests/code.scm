818e9b24b6b3933a8115d65cc49f0df1aca7b4b7
David Wilson <david@daviwil.com>
2020-04-27 17:01:41 -0700

    code: Add C/C++ toolchain and dependencies

--- .config/guix/manifests/code.scm
+++ .config/guix/manifests/code.scm
@@ -3,11 +3,28 @@
 
 (specifications->manifest
  '("node"
-   "python2"
-   "docker-cli"
-   "icedtea"
+
+   ;; C/C++
    "gcc-toolchain"
    "make"
+   "pkg-config"
+
+   ;; Python
+   "python2"
+
+   ;; Docker
+   "docker-cli"
+
+   ;; Java
+   "icedtea"
+
+   ;; SDL
+   "sdl2"
+   "sdl2-image"
+   "sdl2-mixer"
+   "sdl2-gfx"
+   "sdl2-ttf"
+
    "curl"
    "virt-manager"))
    ;; "glibc" ;; For ldd
