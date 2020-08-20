942d36b4791b3590e0b5bdd7b0210095ba00e58c
David Wilson <david@daviwil.com>
2020-08-20 06:55:51 -0700

    systems: Update local Guix repo channel config

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -10,5 +10,10 @@
 ;;         (url "https://gitlab.com/nonguix/nonguix"))
 ;;       (channel
 ;;         (name 'guix)
-;;         (branch "fix-xfce-power-manager-polkit")
-;;         (url "file:///home/daviwil/Projects/Code/guix")))
+;;         (branch "fix-glu-pkg-config")
+;;         (url "file:///home/daviwil/Projects/Code/guix")
+;;         (introduction
+;;           (make-channel-introduction
+;;             "d06d5db885e4b8399e878708862fbe3a67f0592c"
+;;             (openpgp-fingerprint
+;;               "53C4 1E6E 41AA FE55 335A  CA5E 446A 2ED4 D940 BF14")))))
