0224c23e2af85a433dbb82cf84b57da446d96778
David Wilson <david@daviwil.com>
2020-08-28 06:26:26 -0700

    system: The nouveau driver works fine on phantom now

--- .config/guix/systems/phantom.scm
+++ .config/guix/systems/phantom.scm
@@ -8,11 +8,6 @@
  (inherit base-operating-system)
  (host-name "phantom")
 
- ;; The nouveau driver crashes the machine, need to investigate.
- ;; Probably needs PCI-e power management turned off.
- (kernel-arguments
-    '("modprobe.blacklist=nouveau"))
-
  (mapped-devices
   (list (mapped-device
          (source (uuid "091b8ad5-efb3-4c5b-8370-7db99c404a30"))
