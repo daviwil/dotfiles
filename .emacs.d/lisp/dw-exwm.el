2855a5bad6f0bbf4e1012ae439db27c749246b89
David Wilson <david@daviwil.com>
2021-01-06 09:32:37 -0800

    emacs: Commit some old prototyping code for buffer cycling

--- .emacs.d/lisp/dw-exwm.el
+++ .emacs.d/lisp/dw-exwm.el
@@ -1,14 +1,33 @@
 (defvar dw/exwm--floating-pinned-windows '()
   "Holds the set of pinned window IDs.")
 
+(defun dw/next-telega-buffer ()
+  (interactive)
+  (let ((buffers (-filter (lambda (b)
+                            (with-current-buffer b
+                              (eq major-mode "telega-chat-mode")))
+                         (buffer-list))))
+    (message "%s" buffers)))
+
+;; This enables a keybinding to be created for a specific workspace ID which
+;; will switch to the workspace or invoke the buffer cycling function if we're
+;; already on that workspace.
+(defun dw/exwm-switch-or-cycle-workspace (id cycle-func &optional reverse)
+  (interactive)
+  (if (eq exwm-workspace-current-index id)
+      (funcall cycle-func reverse)
+    (exwm-workspace-switch id)))
+
 ;;;###autoload
 (defun dw/exwm-floating-toggle-pinned (&optional id)
   (interactive)
   (when-let ((exwm--floating-frame)
              (window-id (or id exwm--id)))
-    (if (seq-contains dw/exwm--floating-pinned-windows window-id)
-      (setq dw/exwm--floating-pinned-windows (remq window-id dw/exwm--floating-pinned-windows))
-      (push window-id dw/exwm--floating-pinned-windows))))
+    ;; This approach is more reliable for now even with the workspace snap-back
+    (setq exwm--desktop 0xffffffff)))
+    ;; (if (seq-contains dw/exwm--floating-pinned-windows window-id)
+    ;;   (setq dw/exwm--floating-pinned-windows (remq window-id dw/exwm--floating-pinned-windows))
+    ;;   (push window-id dw/exwm--floating-pinned-windows))))
 
 (defun dw/exwm-floating--on-workspace-switch ()
   (let ((current-monitor (frame-parameter exwm--frame 'exwm-randr-monitor)))
