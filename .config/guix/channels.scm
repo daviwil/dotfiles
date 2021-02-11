8a7b5d96d3768ff0734b276915ace6b8acb3e528
David Wilson <david@daviwil.com>
2021-02-11 12:13:54 -0800

    systems: Minor generated file updates

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,24 +1,23 @@
 ;; NOTE: This file is generated from ~/.dotfiles/System.org.  Please see commentary there.
 
-;; (cons* (channel
+(cons* (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix"))
+       %default-channels)
+
+;; (list (channel
 ;;         (name 'nonguix)
 ;;         (commit "c34fa8bfacdce5fa45b2a684c2b27309c09a9056")
 ;;         (url "https://gitlab.com/nonguix/nonguix"))
-;;        %default-channels)
-
-(list (channel
-        (name 'nonguix)
-        (commit "c34fa8bfacdce5fa45b2a684c2b27309c09a9056")
-        (url "https://gitlab.com/nonguix/nonguix"))
-      (channel
-        (name 'guix)
-        (commit "190187326ad7516dd6728eed7bb6ef2d4f92897a")
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-          (openpgp-fingerprint
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+;;       (channel
+;;         (name 'guix)
+;;         (commit "190187326ad7516dd6728eed7bb6ef2d4f92897a")
+;;         (url "https://git.savannah.gnu.org/git/guix.git")
+;;         (introduction
+;;           (make-channel-introduction
+;;             "9edb3f66fd807b096b48283debdcddccfea34bad"
+;;           (openpgp-fingerprint
+;;             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
 
 ;; (list (channel
 ;;         (name 'nonguix)
