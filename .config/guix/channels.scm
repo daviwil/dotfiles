6c9f87b672af6e08bc598be819ab7267cae5d05d
David Wilson <david@daviwil.com>
2021-09-28 15:16:50 -0700

    systems: Add `update-channels` script to save pinned Guix channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,8 +1,18 @@
-;; NOTE: This file is generated from ~/.dotfiles/System.org.  Please see commentary there.
-
 (list (channel
-        (name 'channel-x)
-        (url "file:///home/daviwil/Projects/Code/channel-x"))
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "5edfa6d15e5bb92609ecff7e37e3985eced1dd4d")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (commit
+          "c93654cc7f99e15d5c1252d98e0bbf14e79e7de9"))
       (channel
         (name 'flat)
         (url "https://github.com/flatwhatson/guix-channel.git")
@@ -14,27 +24,7 @@
             (openpgp-fingerprint
               "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
       (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix"))
-      (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        ;; (url "file:///home/daviwil/Projects/Code/guix"))
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
-
-;; (list (channel
-;;         (name 'nonguix)
-;;         (url "https://gitlab.com/nonguix/nonguix"))
-;;       (channel
-;;         (name 'guix)
-;;         (branch "fix-glu-pkg-config")
-;;         (url "file:///home/daviwil/Projects/Code/guix")
-;;         (introduction
-;;           (make-channel-introduction
-;;             "d06d5db885e4b8399e878708862fbe3a67f0592c"
-;;             (openpgp-fingerprint
-;;               "53C4 1E6E 41AA FE55 335A  CA5E 446A 2ED4 D940 BF14")))))
+        (name 'channel-x)
+        (url "file:///home/daviwil/Projects/Code/channel-x")
+        (commit
+          "025eca1931ca5c5489ec3f45a4af7ea558f1bec9")))
