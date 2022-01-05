e96efe1bc8eaa7edfc1c2a8c37706a58a2c90451
David Wilson <david@daviwil.com>
2022-01-05 13:47:27 +0200

    home: Simplify Pipewire dependencies and configuration

--- daviwil/home-services/desktop.scm
+++ daviwil/home-services/desktop.scm
@@ -3,11 +3,11 @@
   #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages fonts)
   #:use-module (gnu packages gnome-xyz)
+  #:use-module (gnu packages linux)
   #:use-module (gnu packages lisp)
   #:use-module (gnu packages wm)
   #:use-module (gnu packages xdisorg)
   #:use-module (daviwil packages fonts)
-  #:use-module (daviwil packages linux)
   #:use-module (flat packages emacs)
   #:use-module (gnu services)
   #:use-module (gnu home services)
@@ -47,7 +47,7 @@ Xft/DPI " (number->string (* 1024 dpi)) " # 1024 * DPI")))))
         emacs-native-comp
 
         ;; Sound
-        pipewire-0.3.38
+        pipewire
 
         ;; Appearance
         compton
@@ -74,25 +74,12 @@ Xft/DPI " (number->string (* 1024 dpi)) " # 1024 * DPI")))))
     (documentation "Run and control gpg-agent.")
     (start #~(make-system-constructor "gpg-connect-agent /bye"))
     (stop #~(make-system-destructor "gpgconf --kill gpg-agent")))
-   ;; Start Pipewire daemons
+   ;; Start Pipewire daemon
    (shepherd-service
-    ;; (requirement '(dbus-home))
     (provision '(pipewire))
     (stop  #~(make-kill-destructor))
     (start #~(make-forkexec-constructor
-              (list #$(file-append pipewire-0.3.38 "/bin/pipewire")))))
-   (shepherd-service
-    (requirement '(pipewire))
-    (provision '(pipewire-media-session))
-    (stop  #~(make-kill-destructor))
-    (start #~(make-forkexec-constructor
-              (list #$(file-append pipewire-0.3.38 "/bin/pipewire-media-session")))))
-   (shepherd-service
-    (requirement '(pipewire))
-    (provision '(pipewire-pulse))
-    (stop  #~(make-kill-destructor))
-    (start #~(make-forkexec-constructor
-              (list #$(file-append pipewire-0.3.38 "/bin/pipewire-pulse")))))))
+              (list #$(file-append pipewire "/bin/pipewire")))))))
 
 (define (home-desktop-environment-variables config)
   '(("_JAVA_AWT_WM_NONREPARENTING" . "1")))
