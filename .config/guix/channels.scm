10664c9a94fc66977826074a2696f2ae4020e581
David Wilson <david@daviwil.com>
2022-06-10 10:48:27 +0300

    systems: Update Guix channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "37e44d48baf976f6bfcd885b2e10da4ab7be4af9")
+          "b483ef1a889ad3f3b523d6f09f810a4618fc753e")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -14,13 +14,13 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "1de0c32142c54bc73af5556d5e45c77152b31f0f"))
+          "bffd70d2b4c81db5148deb6d362b13e5dcbbe150"))
       (channel
         (name 'rde)
         (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
         (commit
-          "ab6931cc1d05c8ab95a1b6c4ab34d0051f7419d7")
+          "8e2d571380f9c2b3878109a8be77d4b60eda216d")
         (introduction
           (make-channel-introduction
             "257cebd587b66e4d865b3537a9a88cccd7107c95"
@@ -31,7 +31,7 @@
         (url "https://github.com/flatwhatson/guix-channel.git")
         (branch "master")
         (commit
-          "094746c1e2e90f2df1e598ab1fd0abb4d75ce84d")
+          "657da22f0229b978b7bf4e4d476f59f17f6a175f")
         (introduction
           (make-channel-introduction
             "33f86a4b48205c0dc19d7c036c85393f0766f806"
@@ -42,4 +42,4 @@
         (url "file:///home/daviwil/Projects/Code/channel-x")
         (branch "master")
         (commit
-          "025eca1931ca5c5489ec3f45a4af7ea558f1bec9")))
+          "0c442c2479fc86fed791e401663d964c999732bf")))
