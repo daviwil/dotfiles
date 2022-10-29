61982ec4b5c78e684824d2335529871ff0d2261d
David Wilson <david@daviwil.com>
2022-10-29 13:34:32 +0300

    systems: Update Guix channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "d921cf8d279daa67406445aed5828564f059600c")
+          "ec74eef890d01a82128ffc9d0188a886c710c8e8")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -14,29 +14,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "ec7c7b852c024095e4f34523452230406a3b4549"))
-      (channel
-        (name 'rde)
-        (url "https://git.sr.ht/~abcdw/rde")
-        (branch "master")
-        (commit
-          "b7de89e274b01a70daa8eb5fb0604308552f4c23")
-        (introduction
-          (make-channel-introduction
-            "257cebd587b66e4d865b3537a9a88cccd7107c95"
-            (openpgp-fingerprint
-              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
-      (channel
-        (name 'flat)
-        (url "https://github.com/flatwhatson/guix-channel.git")
-        (branch "master")
-        (commit
-          "657da22f0229b978b7bf4e4d476f59f17f6a175f")
-        (introduction
-          (make-channel-introduction
-            "33f86a4b48205c0dc19d7c036c85393f0766f806"
-            (openpgp-fingerprint
-              "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+          "d4f3c4b57e05fd3286722ed44b6500093c632258"))
       (channel
         (name 'channel-x)
         (url "file:///home/daviwil/Projects/Code/channel-x")
