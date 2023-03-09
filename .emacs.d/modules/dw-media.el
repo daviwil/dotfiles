fd325996aac56a411ef4aa6bc4f8caaa5f5a21ef
David Wilson <david@daviwil.com>
2023-03-09 20:42:32 +0200

    emacs: Remove emms

--- .emacs.d/modules/dw-media.el
+++ .emacs.d/modules/dw-media.el
@@ -2,15 +2,4 @@
 
 (setup (:pkg mpv :straight t))
 
-(setup (:pkg emms)
-  (require 'emms-setup)
-  (emms-standard)
-  (emms-default-players)
-  (emms-mode-line-disable)
-  (setq emms-source-file-default-directory "~/Music/")
-  (dw/leader-key-def
-    "am"  '(:ignore t :which-key "media")
-    "amp" '(emms-pause :which-key "play / pause")
-    "amf" '(emms-play-file :which-key "play file")))
-
 (provide 'dw-media)
