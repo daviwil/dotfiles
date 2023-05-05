fe2124faaf3f3ee85a8cba5cbf585654f86ff9c8
David Wilson <david@daviwil.com>
2023-05-05 15:09:02 +0300

    emacs: Update god-mode configuration

--- .emacs.d/modules/dw-keys-god.el
+++ .emacs.d/modules/dw-keys-god.el
@@ -1,22 +1,31 @@
+;; -*- lexical-binding: t; -*-
 
-(defun dw/enable-god-mode ()
+(defun dw/god-quit ()
+  "Go back to god-local-mode and quit any active minibuffer."
   (interactive)
-  (god-local-mode 1))
+  (god-local-mode 1)
+  (keyboard-escape-quit))
 
 (use-package god-mode
   :demand t
+  :bind (("<escape>" . dw/god-quit)
+          ("C-x C-1" . delete-other-windows)
+          ("C-x C-2" . split-window-below)
+          ("C-x C-3" . split-window-right)
+          ("C-x C-0" . delete-window)
+          ("C-x C-r" . eval-region)
+          :map god-local-mode-map
+          ("i" . god-local-mode)
+          ("." . repeat))
+
   :config
   (god-mode)
 
-  (global-set-key (kbd "<escape>") #'god-local-mode)
+  (defun dw/god-mode-update-cursor ()
+    "Change cursor type based on god-local-mode."
+    (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
 
-  (define-key god-local-mode-map (kbd "i") 'god-local-mode)
-  (define-key god-local-mode-map (kbd ".") #'repeat)
-  (global-set-key (kbd "C-x C-1") #'delete-other-windows)
-  (global-set-key (kbd "C-x C-2") #'split-window-below)
-  (global-set-key (kbd "C-x C-3") #'split-window-right)
-  (global-set-key (kbd "C-x C-0") #'delete-window)
-  (global-set-key (kbd "C-x C-r") #'eval-region) ;; doesn't work?
+  (add-hook 'post-command-hook #'dw/god-mode-update-cursor)
 
   ;; Other useful keys:
   ;; x j - dired-jump
