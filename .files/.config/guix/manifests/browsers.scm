1d49616f1e5a60cd9f145d43a8f4f11da69da508
David Wilson <david@daviwil.com>
2024-05-14 09:50:14 +0300

    systems: Delete unused Guix manifests

--- .files/.config/guix/manifests/browsers.scm
+++
@@ -1,6 +0,0 @@
-;; This file contains the list of browsers I use occasionally so that I don't
-;; need to install their updates when unnecessary.
-
-(specifications->manifest
- '(;; "ungoogled-chromium"
-   "firefox"))
--- .files/.config/guix/manifests/work.scm
+++
@@ -1,4 +0,0 @@
-;; Packages I need in my day job
-
-(specifications->manifest
- '("freerdp"))
