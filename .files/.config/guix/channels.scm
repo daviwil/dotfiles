7481c8e020316e6f30d78755cfbbc3d7b35e20de
David Wilson <david@daviwil.com>
2024-05-24 09:25:44 +0300

    systems: Update channels

--- .files/.config/guix/channels.scm
+++ .files/.config/guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "7081518be7d2dbb58f3fbfeb1785254a6f0059c8")
+          "34935b49ae20d236b0fc89943f0b1d0d5dea3a33")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -14,7 +14,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "6ba29e02108ed144da1234b4b5512ee03865dcf6")
+          "d7a65f1e910aa41684d4cff62014312420848341")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
