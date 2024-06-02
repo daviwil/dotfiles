2c7372714bd464640ab731beddf159801fbdc6bc
David Wilson <david@daviwil.com>
2024-06-02 10:46:51 +0300

    systems: Use package symbols in home-desktop-service-type

--- daviwil/home-services/desktop.scm
+++ daviwil/home-services/desktop.scm
@@ -1,96 +1,97 @@
 (define-module (daviwil home-services desktop)
   #:use-module (daviwil packages fonts)
+  #:use-module (gnu)
   #:use-module (gnu home services)
   #:use-module (gnu packages)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages glib)
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:export (home-desktop-service-type))
 
+(use-package-modules compression curl fonts freedesktop gimp glib gnome gnome-xyz
+                     gstreamer kde-frameworks linux music package-management
+                     password-utils pdf pulseaudio shellutils ssh syncthing video
+                     web-browsers wget wm xdisorg xorg)
+
 (define (home-desktop-profile-service config)
-  (cons*
-   (list glib "bin")
-   (map specification->package+output
-        '(;; Sway setup
-          "sway"
-          "swayidle"
-          "swaylock"
-          "waybar"
-          "fuzzel"
-          "mako"
-          "gammastep"
-          "grimshot" ;; grimshot --notify copy area
-          "network-manager-applet"
+  (list sway
+        swayidle
+        swaylock
+        waybar
+        fuzzel
+        mako
+        gammastep
+        grimshot ;; grimshot --notify copy area
+        network-manager-applet
 
-          ;; Compatibility for older Xorg applications
-          "xorg-server-xwayland"
+        ;; Compatibility for older Xorg applications
+        xorg-server-xwayland
 
-          ;; Flatpak and XDG utilities
-          "flatpak"
-          "xdg-desktop-portal"
-          "xdg-desktop-portal-gtk"
-          "xdg-desktop-portal-wlr"
-          "xdg-utils" ;; For xdg-open, etc
-          "xdg-dbus-proxy"
-          "shared-mime-info"
+        ;; Flatpak and XDG utilities
+        flatpak
+        xdg-desktop-portal
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-wlr
+        xdg-utils ;; For xdg-open, etc
+        xdg-dbus-proxy
+        shared-mime-info
+        (list glib "bin")
 
-          ;; Appearance
-          "matcha-theme"
-          "papirus-icon-theme"
-          "breeze-icons" ;; For KDE apps
-          "gnome-themes-extra"
-          "adwaita-icon-theme"
+        ;; Appearance
+        matcha-theme
+        papirus-icon-theme
+        breeze-icons ;; For KDE apps
+        gnome-themes-extra
+        adwaita-icon-theme
 
-          ;; Fonts
-          "font-jost"
-          "font-iosevka-ss08"
-          "font-iosevka-aile"
-          "font-jetbrains-mono"
-          "font-google-noto"
-          "font-google-noto-emoji"
-          "font-liberation"
-          "font-awesome"
+        ;; Fonts
+        font-jost
+        font-iosevka-ss08
+        font-iosevka-aile
+        font-jetbrains-mono
+        font-google-noto
+        font-google-noto-emoji
+        font-liberation
+        font-awesome
 
-          ;; Browsers
-          "qtwayland@5"
-          "qutebrowser"
-          "vimb"
+        ;; Browsers
+        (specification->package "qtwayland@5")
+        ;qutebrowser
+        ;vimb
 
-          ;; Authentication
-          "password-store"
+        ;; Authentication
+        password-store
 
-          ;; Audio devices and media playback
-          "mpv"
-          "mpv-mpris"
-          "youtube-dl"
-          "playerctl"
-          "gstreamer"
-          "gst-plugins-base"
-          "gst-plugins-good"
-          "gst-plugins-bad"
-          "gst-plugins-ugly"
-          "gst-libav"
-          "alsa-utils"
-          "pavucontrol"
+        ;; Audio devices and media playback
+        mpv
+        mpv-mpris
+        youtube-dl
+        playerctl
+        gstreamer
+        gst-plugins-base
+        gst-plugins-good
+        gst-plugins-bad
+        gst-plugins-ugly
+        gst-libav
+        alsa-utils
+        pavucontrol
 
-          ;; Graphics
-          "gimp"
+        ;; Graphics
+        gimp-next
 
-          ;; PDF reader
-          "zathura"
-          "zathura-pdf-mupdf"
+        ;; PDF reader
+        zathura
+        zathura-pdf-mupdf
 
-          ;; File syncing
-          "syncthing-gtk"
+        ;; File syncing
+        syncthing-gtk
 
-          ;; General utilities
-          "curl"
-          "wget"
-          "openssh"
-          "zip"
-          "unzip"
-          "trash-cli"))))
+        ;; General utilities
+        curl
+        wget
+        openssh
+        zip
+        unzip
+        trash-cli))
 
 (define (home-desktop-environment-variables config)
   '(("_JAVA_AWT_WM_NONREPARENTING" . "1")))
