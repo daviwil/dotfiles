5ed297c1519d1e74e8996f07cf72fc8ac60a28a8
David Wilson <david@daviwil.com>
2024-06-07 15:40:47 +0300

    emacs: Improve ledger-mode configuration

--- .emacs.d/modules/dw-finance.el
+++ .emacs.d/modules/dw-finance.el
@@ -1,11 +1,18 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package ledger-mode
-  :mode "\\.lgr\\'"
+  :mode "\\.\\(h?ledger\\|journal\\|j\\|lgr\\)$"
   :bind (:map ledger-mode-map
-              ("TAB" . completion-at-point))
+              ("TAB" . completion-at-point)
+              ("M-$" . (lambda ()
+                         (interactive
+                          (insert "€")))))
 
   :custom
+  (ledger-binary-path "hledger")
+  (ledger-mode-should-check-version nil)
+  (ledger-report-auto-width nil)
+  (ledger-report-use-native-highlighting nil)
   (ledger-reports '(("bal" "%(binary) -f %(ledger-file) bal")
                     ("bal this quarter" "%(binary) -f %(ledger-file) --period \"this quarter\" bal")
                     ("bal last quarter" "%(binary) -f %(ledger-file) --period \"last quarter\" bal")
