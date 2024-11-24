582ff10ee3b190d2a196c7a35cebb082d84bddf6
David Wilson <david@daviwil.com>
2024-11-24 10:10:38 +0200

    emacs: Improve initialization of dw-auth module

--- emacs/modules/dw-auth.el
+++ emacs/modules/dw-auth.el
@@ -1,9 +1,13 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; Use `pass` as an auth-source
-;;(auth-source-pass-enable)
+(when (file-exists-p "~/.password-store")
+  (auth-source-pass-enable))
 
 ;; Enable GPG passphrase entry
-(use-package pinentry)
+(use-package pinentry
+  :demand t
+  :config
+  (pinentry-start))
 
 (provide 'dw-auth)
