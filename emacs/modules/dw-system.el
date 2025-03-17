a96ffd02209bdc1d71dc048fcf05df772f2ceb20
David Wilson <david@daviwil.com>
2025-03-17 15:25:15 +0200

    emacs: Update dw-system.el output

--- emacs/modules/dw-system.el
+++ emacs/modules/dw-system.el
@@ -14,3 +14,29 @@
          ("C-c G u s" . dw/guix-update-system)))
 
 (provide 'dw-system)
+
+;; -*- lexical-binding: t; -*-
+(use-package xterm-color
+  :demand t
+  :config
+  ;; Enable ANSI escape handling in eshell
+  (with-eval-after-load 'eshell
+    (add-hook 'eshell-before-prompt-hook
+              (lambda ()
+                (setq xterm-color-preserve-properties t)))
+
+    (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
+    (setq eshell-output-filter-functions (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
+    (setenv "TERM" "xterm-256color"))
+
+  ;; Enable ANSI escape handling in M-x compile
+  (setq compilation-environment '("TERM=xterm-256color"))
+
+  (defun dw/advice-compilation-filter (f proc string)
+    (funcall f proc (xterm-color-filter string)))
+
+  (advice-add 'compilation-filter :around #'dw/advice-compilation-filter))
+
+(use-package eat
+  :config
+  (add-hook 'eshell-load-hook #'eat-eshell-mode))
