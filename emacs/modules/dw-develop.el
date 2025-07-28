e7207f14fd72ebb5660c89f4910408efea118a19
David Wilson <david@daviwil.com>
2025-07-28 11:55:15 +0300

    emacs: Add project tab management with custom prompter

--- emacs/modules/dw-develop.el
+++ emacs/modules/dw-develop.el
@@ -8,6 +8,27 @@
 
 (setq project-compilation-buffer-name-function 'dw/project-compilation-buffer-name-function)
 
+
+(defun dw/ensure-project-in-tab (project-dir)
+  "Ensure we're in a tab named after the project."
+  (let* ((project-name (file-name-nondirectory (directory-file-name project-dir)))
+         (tab-names (mapcar (lambda (tab) (alist-get 'name tab)) (tab-bar-tabs)))
+         (existing-tab (seq-find (lambda (name) (equal name project-name)) tab-names)))
+    (unless existing-tab
+      (tab-new)
+      (tab-rename project-name))
+    (unless (equal (alist-get 'name (tab-bar--current-tab)) project-name)
+      (tab-bar-select-tab-by-name project-name))))
+
+(defun dw/project-prompter ()
+  "Prompt for project and ensure it opens in the correct tab."
+  (let ((project-dir (project-prompt-project-dir)))
+    (dw/ensure-project-in-tab project-dir)
+    project-dir))
+
+;; Use our custom prompter for all project commands
+(setq project-prompter #'dw/project-prompter)
+
 (use-package eglot
   :ensure nil
   :hook ((js-mode
