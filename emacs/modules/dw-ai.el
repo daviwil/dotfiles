52a31a00e427271afe3fcea6784c6c33fbac0ea4
David Wilson <david@daviwil.com>
2026-01-05 06:24:32 +0200

    emacs: Remove monet and claude-code packages

--- emacs/modules/dw-ai.el
+++ emacs/modules/dw-ai.el
@@ -1,19 +1,1 @@
-;; Monet adds some nice integrations into Emacs, like in-editor diffs
-;; of proposed changes and editor context shared when sending prompts
-
-(use-package monet
-  :vc (:url "https://github.com/stevemolitor/monet" :rev :newest))
-
-;; NOTE: This will take a while due to massive GIFs in the repo!
-(use-package claude-code
-  :ensure t
-  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
-  :bind-keymap ("C-c c" . claude-code-command-map)
-  :bind  (:repeat-map my-claude-code-map
-                      ("M" . claude-code-cycle-mode))
-  :config
-  (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
-  (monet-mode 1)
-  (claude-code-mode))
-
 (provide 'dw-ai)
